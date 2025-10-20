b5292e0cf2290fe2bb579dfea3c0999b68b05823
DityaPerdana <athallarafardhan44@gmail.com>
2025-10-20 15:29:13 +0700

    fix redirect

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["https://g7kaih.tefa-bcs.org"],
-  // ...existing config
+  allowedDevOrigins: ["g7kaih.tefa-bcs.org", "www.g7kaih.tefa-bcs.org"],
 };
 
 export default nextConfig;
